5c2514ab86868d04a9f6085b228e6c88dbc06c50
vanduy <chuhaist123@gmail.com>
2023-12-20 23:33:24 +0700

    lab7

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, {useEffect} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import {UserProvider} from './context/UseContext';
+import Router from './Router';
+const Stack = createStackNavigator();
+
+const App = () => {
+  const initial = async () => {
+    const ref = firestore().collection('users');
+
+    const admin = {
+      email: 'chuhaist123@gmail.com',
+      password: 'duynguyen',
+      role: 'admin',
+    };
+
+    await ref.doc(admin.email).onSnapshot(u => {
+      if (!u.exists) {
+        auth()
+          .createUserWithEmailAndPassword(admin.email, admin.password)
+          .then(() => {
+            ref.doc(admin.email).set({
+              ...admin,
+            });
+          });
+      }
+    });
+  };
+
+  useEffect(() => {
+    initial();
+  }, []);
+
+ 
+
+  return (
+    <UserProvider>
+      <NavigationContainer independent={true}>
+        <Router />
+      </NavigationContainer>
+    </UserProvider>
+  );
+};
+
+export default App;
